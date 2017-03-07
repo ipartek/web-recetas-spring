bbfca33d0a2a0fd66c643409c1be08efa00da5cc
anderuraga <ander.uraga@gmail.com>
2017-03-07 16:13:18 +0100

    subimos cobertura test

--- src/test/java/com/ipartek/formacion/domain/IngredienteTest.java
+++ src/test/java/com/ipartek/formacion/domain/IngredienteTest.java
@@ -3,29 +3,62 @@ package com.ipartek.formacion.domain;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class IngredienteTest {
 
+	static final int ID = 2;
+	static final String NOMBRE = "soja";
+	static final boolean NOT_GLUTEN = false;
+
+	static Ingrediente ingrediente = null;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		ingrediente = new Ingrediente();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		ingrediente = null;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
 	@Test
 	public void testConstructor() {
-
-		Ingrediente ingrediente = new Ingrediente();
 
 		assertEquals(-1, ingrediente.getId());
 		assertEquals("", ingrediente.getCantidad());
 		assertEquals("", ingrediente.getNombre());
 		assertTrue(ingrediente.isGluten());
+		ingrediente = null;
 
-		final int id = 2;
-		final String nombre = "soja";
-		final boolean notGluten = false;
+		ingrediente = new Ingrediente(ID, NOMBRE, NOT_GLUTEN);
+		assertEquals(ID, ingrediente.getId());
+		assertEquals(NOMBRE, ingrediente.getNombre());
+		assertEquals(NOT_GLUTEN, ingrediente.isGluten());
+	}
 
-		ingrediente = new Ingrediente(id, nombre, notGluten);
+	@Test
+	public void testSetterGetter() {
 
-		assertEquals(id, ingrediente.getId());
-		assertEquals(nombre, ingrediente.getNombre());
-		assertEquals(notGluten, ingrediente.isGluten());
+		ingrediente.setId(ID);
+		assertEquals(ID, ingrediente.getId());
+
+		ingrediente.setNombre(NOMBRE);
+		assertEquals(NOMBRE, ingrediente.getNombre());
+
 	}
 
 }
