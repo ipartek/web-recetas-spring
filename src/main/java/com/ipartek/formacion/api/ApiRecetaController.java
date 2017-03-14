57ec0c063061dbcfc6b8f26e3d9414eb85fe12ab
anderuraga <ander.uraga@gmail.com>
2017-03-14 10:06:47 +0100

    nuevo servicio alta receta, TODO mejorar codigos de estdo y validaciones

--- src/main/java/com/ipartek/formacion/api/ApiRecetaController.java
+++ src/main/java/com/ipartek/formacion/api/ApiRecetaController.java
@@ -2,19 +2,25 @@ package com.ipartek.formacion.api;
 
 import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ipartek.formacion.domain.Receta;
+import com.ipartek.formacion.domain.Usuario;
 import com.ipartek.formacion.service.ServiceReceta;
 
 @Controller
 @RequestMapping(value = "/api/receta/")
 public class ApiRecetaController {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ApiRecetaController.class);
 
 	@Autowired
 	ServiceReceta servideReceta;
@@ -22,7 +28,8 @@ public class ApiRecetaController {
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public @ResponseBody ArrayList<Receta> listar() {
 
-		return (ArrayList<Receta>) this.servideReceta.listar();
+		// TODO Fallo seguridad mostrar informacion del usuario privada
+		return (ArrayList<Receta>) this.servideReceta.listarConUsuarios();
 	}
 
 	@RequestMapping(value = "{id}", method = RequestMethod.GET)
@@ -32,6 +39,22 @@ public class ApiRecetaController {
 
 		Receta receta = this.servideReceta.buscarPorID(id);
 		return receta;
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public void altaReceta(@RequestBody Receta receta) {
+
+		LOG.info("Dar de alta nueva receta: " + receta);
+
+		// TODO validar datos
+
+		// TODO siempre usamos el mismo usuario para las recetas
+		Usuario client = new Usuario();
+		client.setId(1);
+		receta.setUsuario(client);
+
+		boolean alta = this.servideReceta.crear(receta);
+
 	}
 
 }
